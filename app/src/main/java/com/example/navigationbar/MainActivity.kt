b9ed469769a14dacd919829603bd11770b1a3777
dalivalovinika <nika.dalivalovi.1@btu.edu.ge>
2020-12-10 04:22:11 +0400

    Hi !

---
+++ app/src/main/java/com/example/navigationbar/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.navigationbar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfig = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_myownfragment
+        ))
+
+        setupActionBarWithNavController(navController, appBarConfig)
+        navView.setupWithNavController(navController)
+
+    }
+}
+
---
+++ app/src/main/java/com/example/navigationbar/fragments/MyFragment.kt
@@ -0,0 +1,7 @@
+package com.example.navigationbar.fragments
+
+import androidx.fragment.app.Fragment
+
+class MyFragment: Fragment() {
+    
+}
